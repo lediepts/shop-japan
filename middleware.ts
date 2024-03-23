10e8b0778488b9ec3b554a4c129b953bdf5c3c37
diep4s <diep.le@0004s.com>
2024-03-23 17:37:10 +0900

    ,,,..

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const allowedOrigins = [
-  "*"
-];
+const allowedOrigins = ["https://hoaisunny.vercel.app"];
 
 const corsOptions = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
